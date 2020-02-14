3ed81f83807ac4204cb6eef242bc8475d310a566
JViggiani <josh.viggiani@blueyonder.co.uk>
2020-02-14 15:14:52 +0000

    File parsed into data structure

---
+++ FastQ Project/Fragment.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct Fragment
+{
+	char *aSeqId;
+	char *aRawSequence;
+	//remember to append a + to line 3
+	char *aQualityValue;
+};
--- FastQ Project/main.cpp
+++ FastQ Project/main.cpp
@@ -3,10 +3,60 @@
 
 #include "pch.h"
 #include <iostream>
+#include <fstream>
+
+#include "Fragment.h"
+
+Fragment* populateNextFragment(std::ifstream& aInputStream)
+{
+	Fragment* aFragment = new Fragment();
+	int aFragmentLineNum = 1;
+	for (string line; 
+		aFragmentLineNum <= 4 && getline(aInputStream, line) && !aInputStream.eof(); 
+		++aFragmentLineNum)
+	{
+		cout << line << "\n";
+
+		//File may contain blank lines, added for some resilience
+		if (line.size() == 0)
+		{
+			--aFragmentLineNum;
+			continue;
+		}
+
+		switch (aFragmentLineNum)
+		{
+		case 1:
+			aFragment->aSeqId = new char[line.length() + 1];
+			strcpy(aFragment->aSeqId, line.c_str());
+			break;
+		case 2:
+			aFragment->aRawSequence = new char[line.length() + 1];
+			strcpy(aFragment->aRawSequence, line.c_str());
+			break;
+		case 3:
+			break;
+		case 4:
+			aFragment->aQualityValue = new char[line.length() + 1];
+			strcpy(aFragment->aQualityValue, line.c_str());
+			break;
+		}
+	}
+
+	if (aFragment && aFragment->aSeqId && aFragment->aRawSequence && aFragment->aQualityValue)
+	{
+		//Only want to return complete fragments
+		return aFragment;
+	}
+	else return nullptr;
+}
+
+//This is not neccessarily a fixed length in real data but it is 
+
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	//std::cout << "Hello World!\n";
 	//https://stackoverflow.com/questions/41245157/unable-to-sort-a-large-file-containing-integers
 	//https://stackoverflow.com/questions/30618424/c-reading-large-files-part-by-part
 	//https://stackoverflow.com/questions/22101347/parsing-large-file-in-c
@@ -22,6 +72,58 @@ int main()
 		foreach item in multiset, start blurting out the data inside 
 		delete object from memory as you go
 	*/
+
+	//what we know about the format:
+	/*
+	mate pairs - opposite ends of the same dna fragment
+		same name with a trailing /1 or /2 indicator
+		these occur adjacently
+	base quality: each segment has a quality dictated by arbitrary characters - interpret as binary
+	four lines:
+		some random machine or date information, but importantly contains "/1" or "/2"
+		DNA 
+		+
+		Quality
+	*/
+
+	string aFileName = "data/tiny-test.fastq";
+
+	std::ifstream aInputStream(aFileName);
+
+	//string aLine;
+
+	//getline(aInputStream, aLine);
+
+	//cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	/*
+	Fragment* aFragment = new Fragment();
+	//Sequence Id
+	aInputStream.getline(aFragment->aSeqId, aSeqIdLength);
+	//Sequence
+	string aSeq;
+	getline(aInputStream, aSeq);
+	aFragment->aRawSequence = new char[aSeq.length()+1];
+	strcpy(aFragment->aRawSequence, aSeq.c_str());
+
+	cout << aFragment->aSeqId;
+	cout << aFragment->aRawSequence;
+	*/
+
+	vector<Fragment*> aFragmentVector;
+
+	while (!aInputStream.eof())
+	{
+		Fragment* aFragment1 = populateNextFragment(aInputStream);
+		if (aFragment1)
+		{
+			aFragmentVector.push_back(aFragment1);
+		}
+	}
+
+	cout << aFragmentVector.size();
+
+	aInputStream.close();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
